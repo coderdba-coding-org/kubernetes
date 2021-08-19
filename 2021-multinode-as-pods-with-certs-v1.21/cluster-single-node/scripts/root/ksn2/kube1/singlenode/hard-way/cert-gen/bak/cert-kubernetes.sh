b2c5c2ea0b96ef53d9227bc6b60d55a98f47f8ac
coderdba <gowrishdba@yahoo.com>
2021-08-19 20:55:04 +0530

    more changes

---
+++ 2021-multinode-as-pods-with-certs-v1.21/cluster-single-node/scripts/root/ksn2/kube1/singlenode/hard-way/cert-gen/bak/cert-kubernetes.sh
@@ -0,0 +1,33 @@
+{
+
+KUBERNETES_PUBLIC_ADDRESS=192.168.99.102
+KUBERNETES_HOSTNAMES=ksn2,kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
+
+cat > kube-apiserver-csr.json <<EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=10.96.0.1,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} \
+  -profile=kubernetes \
+  kube-apiserver-csr.json | cfssljson -bare kube-apiserver
+
+}
---
+++ 2021-multinode-as-pods-with-certs-v1.21/cluster-single-node/scripts/root/ksn2/kube1/singlenode/hard-way/etcd/osfiles/etc/systemd/system/README.txt
@@ -0,0 +1,2 @@
+'Listen' kind of url's gave problem when actual IPs were used instead of 0.0.0.0
+- Therefore, using 0.0.0.0 
