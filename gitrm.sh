dd8a35a9f7c9bd47543541f052ca374bd827b9e5
coderdba <coderdba@coder.com>
2021-10-07 17:15:31 +0530

    updates

---
+++ gitrm.sh
@@ -0,0 +1,7 @@
+git rm    2021-single-node-hard-way-v1.21-trial2/firewall/firewall-master.sh
+git rm    2021-single-node-hard-way-v1.21-trial2/firewall/firewall-worker.sh
+git rm    2021-single-node-hard-way-v1.21-trial2/kube/.README.txt.swp
+git rm    2021-single-node-hard-way-v1.21-trial2/kube/clusterroles/kube-apiserver-to-kubelet-apply-bind.sh
+git rm    2021-single-node-hard-way-v1.21-trial2/kube/clusterroles/kube-apiserver-to-kubelet-apply.sh
+git rm    2021-single-node-hard-way-v1.21-trial2/kube/osfiles/var/lib/kubelet/cpu_manager_state
+
